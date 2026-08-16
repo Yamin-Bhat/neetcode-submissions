22c01a68a4d75561b8e7a382aa00754c8c315c7e
Yamin Mohammad Bhat <yaminbhat088@gmail.com>
2026-08-16 09:08:06 +0530

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int high = n - 1 , low = 0;
+        int mid;
+
+        // if(n == 1 && target == nums[0]){
+        //     return 0;
+        // }
+        while(low <= high){
+            mid = (high  + low) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[mid] > target){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
